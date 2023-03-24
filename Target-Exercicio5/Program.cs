92f5179db5f1ca6934048f7ab70763fd0c573233
willtsuchida <willian_hideki@hotmail.com>
2023-03-24 16:44:51 -0300

    Target - Exercicio 5

---
+++ Target-Exercicio5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Digite uma palavra ou frase: ");
+String dado = Console.ReadLine();
+String dadoInverso;
+
+char[] arrayOrdenado = dado.ToCharArray();
+int tamanhoArray = arrayOrdenado.Length;
+char[] arrayInverso = new char[tamanhoArray];
+
+for (int i = 0; i < arrayOrdenado.Length; i++)
+{
+    arrayInverso[tamanhoArray - 1] = arrayOrdenado[i];
+    tamanhoArray--;
+}
+
+dadoInverso = new string(arrayInverso);
+
+Console.WriteLine(dadoInverso);
